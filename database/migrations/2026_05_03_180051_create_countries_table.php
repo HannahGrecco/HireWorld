a7977871a0a3e993a081833259d762811e786dc3
HannahGrecco <107350125+HannahGrecco@users.noreply.github.com>
2026-05-03 15:35:00 -0300

    Installing laravel breeze and creating first migration

---
+++ database/migrations/2026_05_03_180051_create_countries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->char('iso_code', 2)->unique;
+            $table->string('region', 100)->nullable;
+            $table->char('currency_code', 3)->nullable;
+            $table->string('official_language', 100)->nullable;
+            $table->string('timezone', 100)->nullable;
+            $table->string('flag_emoji', 10)->nullable;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
